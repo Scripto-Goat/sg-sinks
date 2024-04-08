fe0955cefc19af8a03378c184bc2fe5c43f55f21
Randolio <nobanpls@mail.com>
2024-04-08 15:43:51 +0100

    refactor(client/config)
    - Fix indentation issues.
    - Utilize lib require for config.
    - Switch Config.Sound to use boolean rather than a string.

--- bridge/client.lua
+++ bridge/client.lua
@@ -1,4 +1,3 @@
-
 -- Notifications
 function Notify(title, msg, type)
     if type == 'success' then
@@ -15,19 +14,19 @@ function Notify(title, msg, type)
             exports['mythic_notify']:DoHudText('success', msg)
         end
 
-elseif type == 'error' then
+    elseif type == 'error' then
 
-    if Config.Notify == 'okok' then
-        exports['okokNotify']:Alert(title, msg, 3000, 'error', true)
-    elseif Config.Notify == 'ox' then
-        lib.notify({
-            title = title,
-            description = msg,
-            type = 'error'
-        })
-    elseif Config.Notify == 'mythic' then
-        exports['mythic_notify']:DoHudText('error', msg)
+        if Config.Notify == 'okok' then
+            exports['okokNotify']:Alert(title, msg, 3000, 'error', true)
+        elseif Config.Notify == 'ox' then
+            lib.notify({
+                title = title,
+                description = msg,
+                type = 'error'
+            })
+        elseif Config.Notify == 'mythic' then
+            exports['mythic_notify']:DoHudText('error', msg)
+        end
+
     end
-    end
-    
 end
--- client.lua
+++ client.lua
@@ -1,10 +1,10 @@
-
+local Config = lib.require('config')
 -- Wash hands
 local function Washhands()
 
     if Config.Progress.Type == 'circle' then
 
-        if Config.Sound == 'true' then
+        if Config.Sound then
             TriggerServerEvent("InteractSound_SV:PlayWithinDistance", 6, "hands", 0.5)
         end
     
@@ -40,7 +40,7 @@ local function Washhands()
         end
     elseif Config.Progress.Type == 'bar' then
 
-        if Config.Sound == 'true' then
+        if Config.Sound then
             TriggerServerEvent("InteractSound_SV:PlayWithinDistance", 8, "hands", 0.5)
         end
 
@@ -61,63 +61,62 @@ local function Washhands()
             },
         }) 
         then
-        Wait(100)
-        Notify(Locals[Config.Language]['SuccessTitle'], Locals[Config.Language]['SuccessDescription'], 'success')
-    else
-        ClearPedTasksImmediately(cache.ped)
-        Notify(Locals[Config.Language]['ErrorTitle'], Locals[Config.Language]['ErrorDescription'], 'error')
-    end
-elseif Config.Progress.Type == 'skillcheck' then
+            Wait(100)
+            Notify(Locals[Config.Language]['SuccessTitle'], Locals[Config.Language]['SuccessDescription'], 'success')
+        else
+            ClearPedTasksImmediately(cache.ped)
+            Notify(Locals[Config.Language]['ErrorTitle'], Locals[Config.Language]['ErrorDescription'], 'error')
+        end
 
-    local success = lib.skillCheck({'easy'}, {'e'})
+    elseif Config.Progress.Type == 'skillcheck' then
 
-    if success then
+        local success = lib.skillCheck({'easy'}, {'e'})
 
-        if Config.Sound == 'true' then
+        if success then
+
+            if Config.Sound then
+                TriggerServerEvent("InteractSound_SV:PlayWithinDistance", 4, "hands", 0.5)
+            end
+
+            TaskPlayAnim(cache.ped,"missheist_agency3aig_23","urinal_sink_loop", 1.0, -1.0, -1, 1, 1, true, true, true)
+            Wait(6500)
+            ClearPedTasksImmediately(cache.ped)
+
+            Notify(Locals[Config.Language]['SuccessTitle'], Locals[Config.Language]['SuccessDescription'], 'success')
+
+        else
+            lib.cancelSkillCheck()
+            ClearPedTasksImmediately(cache.ped)
+            Notify(Locals[Config.Language]['ErrorTitle'], Locals[Config.Language]['ErrorDescription'], 'error')
+        end
+    elseif Config.Progress.Type == 'qb-bar' then
+
+        if Config.Sound then
             TriggerServerEvent("InteractSound_SV:PlayWithinDistance", 4, "hands", 0.5)
         end
 
-        TaskPlayAnim(cache.ped,"missheist_agency3aig_23","urinal_sink_loop", 1.0, -1.0, -1, 1, 1, true, true, true)
-        Wait(6500)
-        ClearPedTasksImmediately(cache.ped)
-
-        Notify(Locals[Config.Language]['SuccessTitle'], Locals[Config.Language]['SuccessDescription'], 'success')
-
-    else
-        lib.cancelSkillCheck()
-        ClearPedTasksImmediately(cache.ped)
-        Notify(Locals[Config.Language]['ErrorTitle'], Locals[Config.Language]['ErrorDescription'], 'error')
-    end
-elseif Config.Progress.Type == 'qb-bar' then
-
-    if Config.Sound == 'true' then
-        TriggerServerEvent("InteractSound_SV:PlayWithinDistance", 4, "hands", 0.5)
-    end
-
-    TaskStartScenarioInPlace(cache.ped, Config.Animation, 0, true)
-    local QBCore = exports['qb-core']:GetCoreObject()
-    QBCore.Functions.Progressbar("washhands", Locals[Config.Language]['ProgressLabel'], Config.Progress.Duration, false, true, {
-        disableMovement = true,
-        disableCarMovement = true,
-        disableMouse = false,
-        disableCombat = true,
-     }, {
-        animDict = "missheist_agency3aig_23",
-        anim = "urinal_sink_loop",
-     }, {}, {}, function()
-        Notify(Locals[Config.Language]['SuccessTitle'], Locals[Config.Language]['SuccessDescription'], 'success')
-     end, function()
-        ClearPedTasksImmediately(cache.ped)
-        Notify(Locals[Config.Language]['ErrorTitle'], Locals[Config.Language]['ErrorDescription'], 'error')
-     end)
+        TaskStartScenarioInPlace(cache.ped, Config.Animation, 0, true)
+        local QBCore = exports['qb-core']:GetCoreObject()
+        QBCore.Functions.Progressbar("washhands", Locals[Config.Language]['ProgressLabel'], Config.Progress.Duration, false, true, {
+            disableMovement = true,
+            disableCarMovement = true,
+            disableMouse = false,
+            disableCombat = true,
+            }, {
+            animDict = "missheist_agency3aig_23",
+            anim = "urinal_sink_loop",
+            }, {}, {}, function()
+            Notify(Locals[Config.Language]['SuccessTitle'], Locals[Config.Language]['SuccessDescription'], 'success')
+            end, function()
+            ClearPedTasksImmediately(cache.ped)
+            Notify(Locals[Config.Language]['ErrorTitle'], Locals[Config.Language]['ErrorDescription'], 'error')
+        end)
     end
 end
 
 -- Target
 CreateThread(function()
-
     if Config.Target.Type == 'ox' then
-
         exports.ox_target:addModel(Config.Props, {
             {
                 name = 'sink',
@@ -131,24 +130,20 @@ CreateThread(function()
                 end
             }
         })
-
     elseif Config.Target.Type == 'qb' then
-
-          exports['qb-target']:AddTargetModel(Config.Props, {
-            options = { 
-              { 
-                label = Locals[Config.Language]['TargetLabel'], 
-                icon = Config.Target.Icon,
-                --item = Config.Target.Item, -- Uncomment if you only want people to see target when they have item in inventory, also uncomment in config.lua
-                type = "client", 
-                action = function()
-                    Washhands()
-                end
-              }
-            },
-            distance = Config.Target.Distance, 
-          })
-
+        exports['qb-target']:AddTargetModel(Config.Props, {
+        options = { 
+            { 
+            label = Locals[Config.Language]['TargetLabel'], 
+            icon = Config.Target.Icon,
+            --item = Config.Target.Item, -- Uncomment if you only want people to see target when they have item in inventory, also uncomment in config.lua
+            type = "client", 
+            action = function()
+                Washhands()
+            end
+            }
+        },
+        distance = Config.Target.Distance, 
+        })
     end
-
 end)
